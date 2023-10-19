b548ea8557d8e4c3716ff442ffb94f38297e9d65
hong <service@teamcircle.io>
2023-10-19 18:25:52 +0800

    Bug fixes

--- TCSDK/TeamCircleSDK.framework/Headers/TCCircleHomeVCtrl.h
+++ TCSDK/TeamCircleSDK.framework/Headers/TCCircleHomeVCtrl.h
@@ -14,6 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)setViewLayout;
 - (void)setFeedToTop;
 - (void)setNavigationController:(UINavigationController *)navigationController;
+- (void)changeTopView;
 
 @end
 
